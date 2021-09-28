30b1f8660c10ba96585cd10fd0807f30f001d90b
pikokr <parangee9706@gmail.com>
2021-09-28 16:06:05 +0900

    exclude privileged intents

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -2,11 +2,13 @@ import { CommandClient } from '@pikokr/command.ts'
 import Discord, { Intents, IntentsString } from 'discord.js'
 import { config } from '../config'
 
+const excludedIntents: IntentsString[] = ['GUILD_MEMBERS', 'GUILD_PRESENCES']
+
 export class Client extends CommandClient {
     constructor() {
         super({
             client: new Discord.Client({
-                intents: Object.keys(Intents.FLAGS) as IntentsString[],
+                intents: (Object.keys(Intents.FLAGS) as IntentsString[]).filter((x)=>!excludedIntents.includes(x)),
             }),
             owners: 'auto',
             command: {
