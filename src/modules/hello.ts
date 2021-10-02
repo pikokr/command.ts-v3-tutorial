d45a3465abfe28f80ba7fef624273aac0ef1ec19
pikokr <parangee9706@gmail.com>
2021-10-02 11:26:21 +0900

    ep05

--- src/modules/hello.ts
+++ src/modules/hello.ts
@@ -1,21 +1,45 @@
 import { command, listener, Module, rest } from '@pikokr/command.ts'
 import { Client } from '../structures/client'
-import { Message } from 'discord.js'
+import { ButtonInteraction, Message, MessageButton } from 'discord.js'
+import { generateComponents, uiComponent, View } from '@pikokr/command.ts-views'
+
+class HelloView extends View {
+    @uiComponent(new MessageButton().setStyle('PRIMARY').setLabel('테스트'), { deferUpdate: false })
+    async test(i: ButtonInteraction) {
+        await i.reply('와아아')
+    }
+}
 
 class Hello extends Module {
+    helloView: HelloView
+
     constructor(private cts: Client) {
         super()
+        this.helloView = new HelloView()
     }
 
-    @command({name: '말하기'})
+    @command({ name: '말하기' })
     async say(msg: Message, @rest content: string) {
         if (!content) content = '내용이 없네요!'
         await msg.channel.send({
             content,
             allowedMentions: {
-                parse: []
-            }
+                parse: [],
+            },
         })
+    }
+
+    @command({ name: 'view' })
+    async view(msg: Message) {
+        await msg.reply({
+            content: 'views 모듈 테스트!',
+            components: generateComponents(this.helloView),
+        })
+    }
+
+    @listener('commandError')
+    err(err: Error) {
+        console.error(err)
     }
 
     @listener('ready')
--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,6 +1,7 @@
 import { CommandClient } from '@pikokr/command.ts'
 import Discord, { Intents, IntentsString } from 'discord.js'
 import { config } from '../config'
+import { setup } from '@pikokr/command.ts-views'
 
 const excludedIntents: IntentsString[] = ['GUILD_MEMBERS', 'GUILD_PRESENCES']
 
@@ -8,7 +9,7 @@ export class Client extends CommandClient {
     constructor() {
         super({
             client: new Discord.Client({
-                intents: (Object.keys(Intents.FLAGS) as IntentsString[]).filter((x)=>!excludedIntents.includes(x)),
+                intents: (Object.keys(Intents.FLAGS) as IntentsString[]).filter((x) => !excludedIntents.includes(x)),
             }),
             owners: 'auto',
             command: {
@@ -19,6 +20,8 @@ export class Client extends CommandClient {
                 guild: config.slash.guild,
             },
         })
+
+        setup(this)
 
         this.registry.loadModulesIn('modules')
     }
